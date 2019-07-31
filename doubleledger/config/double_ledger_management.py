afc653b95a9567fe30ddc8abd149b448df985fe5
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-07-31 18:34:00 +0500

    Bug Fix

--- doubleledger/config/double_ledger_management.py
+++ doubleledger/config/double_ledger_management.py
@@ -5,13 +5,13 @@ from frappe import _
 def get_data():
     return[
         {
-            "lable": ("Setup"),
+            "label": ("Setup"),
             "items": [
 				{
 					"type": "doctype",
 					"name": "Double Ledger Parties",
 					"onboard": 1,
-					"lable": _("Double Ledger Parties"),
+					"label": _("Double Ledger Parties"),
 					"description": _(" Managing Parties that act as both customers and suppliers"),
 				}
 			]
