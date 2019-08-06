417a6ea4ce28c42b956514aaa6d4f50f8f176d53
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-08-06 23:24:03 +0500

    Bug Fixed

--- doubleledger/double_ledger_management/doctype/double_ledger_parties/double_ledger_parties.py
+++ doubleledger/double_ledger_management/doctype/double_ledger_parties/double_ledger_parties.py
@@ -70,7 +70,7 @@ def create_invoice_adj_jv(self,cdt):
     
             customer_account_type = get_party_account('Customer', customer_dp, doc.company)
             supplier_account_type = get_party_account('Supplier', supplier_dp, doc.company)
-
+            
             jv = frappe.new_doc('Journal Entry')
             jv.voucher_type = 'Journal Entry'
             jv.naming_series = 'JV-'
@@ -101,15 +101,43 @@ def create_invoice_adj_jv(self,cdt):
 
 def cancel_adjusted_jv(self,cdt):
     doc = frappe.get_doc(self,cdt)
-    jv_list = frappe.get_list('Journal Entry', filters= [['remark','like','%{0}%'.format(doc.name)]], fields = "*" )
-    jv_name = None
-    if(jv_list!=None):
-        for msg in jv_list:
-            jv_name = msg.name
-        
-        jv_doc = frappe.get_doc('Journal Entry', jv_name)
-        jv_doc.cancel()
-        frappe.msgprint("Journal Entry " + jv_name + " cancelled")
+    if self.doctype == 'Sales Invoice' and self.is_pos == 0 and self.no_double_ledger==0:
+        doc = frappe.get_doc(self,cdt)
+        customer_list = frappe.get_list('Double Ledger Parties', filters= {'primary_role':'Supplier','customer':self.customer}, fields = "*" )
+        customer_dp = None
+        supplier_dp = None
+        primary_role_dp = None
+
+        if customer_list:
+            jv_list = frappe.get_list('Journal Entry', filters= [['remark','like','%{0}%'.format(doc.name)],['docstatus','=','1']], fields = "*" )
+            jv_name = None
+            if(jv_list!=None):
+                for msg in jv_list:
+                    jv_name = msg.name
+                    
+                if jv_name!=None:
+                    jv_doc = frappe.get_doc('Journal Entry', jv_name)
+                    jv_doc.cancel()
+                    frappe.msgprint("Journal Entry " + jv_name + " cancelled")
+
+    elif self.doctype == 'Purchase Invoice' and self.is_paid == 0 and self.no_double_ledger==0: 
+        doc = frappe.get_doc(self,cdt)
+        supplier_list = frappe.get_list('Double Ledger Parties', filters= {'primary_role':'Customer','supplier':self.supplier}, fields = "*" )
+        customer_dp = None
+        supplier_dp = None
+        primary_role_dp = None
+
+        if supplier_list:
+            jv_list = frappe.get_list('Journal Entry', filters= [['remark','like','%{0}%'.format(doc.name)],['docstatus','=','1']], fields = "*" )
+            jv_name = None
+            if(jv_list!=None):
+                for msg in jv_list:
+                    jv_name = msg.name
+                    
+                if jv_name!=None:
+                    jv_doc = frappe.get_doc('Journal Entry', jv_name)
+                    jv_doc.cancel()
+                    frappe.msgprint("Journal Entry " + jv_name + " cancelled")            
 
 
 def prevent_linked_jv_cancellation(self,cdt):
