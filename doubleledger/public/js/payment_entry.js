0ed704192061242827a536d5927a9b8184e80247
Muhammad Ahmad <ahmad@dev.mshome.net>
2020-01-14 12:22:29 +0500

    minor bug fix in Payment entry

--- doubleledger/public/js/payment_entry.js
+++ doubleledger/public/js/payment_entry.js
@@ -1,18 +1,20 @@
 frappe.ui.form.on('Payment Entry',{
 	party: function(frm){
-		frappe.call({
-			method: "doubleledger.utils.payment_entry.get_party_primary_role",
-			args: {
-				party_type: frm.doc.party_type,
-				party: frm.doc.party 
-			},
-			callback:function(r){
-				frm.doc.primary_role = r.message
-				frm.refresh_field('primary_role')
-				if (frm.doc.party_type!= frm.doc.primary_role){
-					frappe.msgprint('Party Type and Primary Role does not match, please double check your transaction!')
+		if (frm.doc.party)	{
+				frappe.call({
+				method: "doubleledger.utils.payment_entry.get_party_primary_role",
+				args: {
+					party_type: frm.doc.party_type,
+					party: frm.doc.party 
+				},
+				callback:function(r){
+					frm.doc.primary_role = r.message
+					frm.refresh_field('primary_role')
+					if (frm.doc.party_type!= frm.doc.primary_role && frm.doc.primary_role){
+						frappe.msgprint('Party Type and Primary Role does not match, please double check your transaction!')
+					}
 				}
-			}
-		})
+			})
+		}
 	}
 })
